ab750da60bd615c5c2ccd4efa4dbe885c502134d
James Nunn <jamesnunn@jbarisk.com>
2017-04-17 22:40:04 +0100

    getting openlayers to display stations

--- stations/views.py
+++ stations/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+from django.core.serializers import serialize
+
+from stations.models import Stations
 
 # Create your views here.
 
 def index(request):
-    return render_to_response('index.html')
+    stations = serialize('geojson', Stations.objects.all(),
+          geometry_field='point',
+          fields=('label',))
+    return render_to_response('index.html', {'stations': stations})
