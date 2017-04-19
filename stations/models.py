3255e79dbf8c5d9a2d2eda053a1968f0106707d5
James Nunn <jamesnunn@jbarisk.com>
2017-04-19 23:02:57 +0100

    added models for alerts

--- stations/models.py
+++ stations/models.py
@@ -1,4 +1,6 @@
-from django.contrib.gis.db import models as models
+from django.contrib.gis.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class Stations(models.Model):
@@ -15,3 +17,36 @@ class Stations(models.Model):
 
     def __str__(self):
         return station_ref
+
+
+class Units(models.Model):
+    unit = models.CharField(max_length=5, unique=True)
+
+
+class ReadingTypes(models.Model):
+    reading_type = models.CharField(max_length=20, unique=True)
+
+
+class ReadingConditionTypes(models.Model):
+    reading_condition_type = models.CharField(max_length=50, unique=True)
+
+
+class StationReadings(models.Model):
+    station_id = models.ForeignKey(Stations, on_delete=models.CASCADE)
+    measure = models.FloatField()
+    units = models.ForeignKey(Units)
+    datetime = models.DateTimeField()
+    reading_type = models.ForeignKey(ReadingTypes)
+
+
+class Alert(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    station_id = models.ForeignKey(Stations, on_delete=models.CASCADE)
+    reading_type_id = models.ForeignKey(ReadingTypes)
+    reading_condition_type_id = models.ForeignKey(ReadingConditionTypes)
+    reading_min = models.FloatField(null=True)
+    reading_max = models.FloatField(null=True)
+
+    def clean(self):
+        if self.reading_min is None and self.reading_max is None:
+            raise ValidationError('There must be at least one reading.')
