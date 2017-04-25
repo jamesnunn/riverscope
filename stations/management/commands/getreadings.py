ef558c16ec250fecd860e26ae5f0b9697cf3a77f
James Nunn <jamesnunn@jbarisk.com>
2017-04-25 21:49:51 +0100

    fixed issue where one reading only was being added to db

--- stations/management/commands/getreadings.py
+++ stations/management/commands/getreadings.py
@@ -17,24 +17,26 @@ LOG = logger.FilePrintLogger(__name__)
 
 def get_readings(station):
     url = utils.readings_url(measure=station.measure, sort=True, limit=10)
+    station_readings = []
     try:
         measures = utils.get_url_json_response(url)
         for measure in measures['items']:
             date = datetime.strptime(measure['dateTime'], '%Y-%m-%dT%H:%M:%SZ')
             date = date.replace(tzinfo=timezone.utc)
             value = float(measure['value'])
-            return StationReadings(station=station, datetime=date, measure=value)
+            station_readings.append(StationReadings(station=station, datetime=date, measure=value))
             # StationReadings.objects.create(station=station, datetime=date, measure=value)
+        return station_readings
     except Exception as err:
-        LOG.warning('Skipped measures for {}: {}'.format(station.station_ref, str(err)))
+        LOG.warning('Skipped reading for {}: {}'.format(station.station_ref, str(err)))
 
 
 class Command(BaseCommand):
     help = 'Get latest readings for stations.'
 
     def add_arguments(self, parser):
-        parser.add_argument('-n', '--lastn', type=int, default=5,
-                            help='Get last n measures.')
+        parser.add_argument('-n', '--lastn', type=int, default=10,
+                            help='Get last n readings.')
         parser.add_argument('-d', '--debug', action='store_true',
                             help='Run in debug mode')
         parser.add_argument('-l', '--log', help='Set the log directory',
@@ -54,9 +56,10 @@ class Command(BaseCommand):
         time_start = utils.start_timer()
         pool = ThreadPool(100)
         # TODO http://stackoverflow.com/questions/2632520/what-is-the-fastest-way-to-send-100-000-http-requests-in-python
-        results = pool.map(get_readings, Stations.objects.all())
+        results = list(filter(None, pool.map(get_readings, Stations.objects.all())))
+        station_readings = [s for sl in results for s in sl]
         with transaction.atomic():
             StationReadings.objects.all().delete()
-            StationReadings.objects.bulk_create(results)
+            StationReadings.objects.bulk_create(station_readings)
         time_diff = utils.end_timer(time_start)
-        LOG.info('Added {} readings in {}'.format(len(results), time_diff))
+        LOG.info('Added {} readings in {}'.format(len(station_readings), time_diff))
